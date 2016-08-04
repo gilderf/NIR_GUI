a7f1a0fb7114598025619658c198b9e97332aaf7
mithp <mithilesh.prakash@uef.fi>
2016-08-04 12:05:55 +0300

    Cleaning unwanted files

--- summer.m
+++
@@ -1,9 +0,0 @@
-function [ x1,x2,x3,x4 ] = summer(inp1,inp2,inp3,inp4)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-x1= inp1 * 2;
-x2= inp2 * 3;
-x3= inp3 * 1;
-x4= inp4 * 1;
-end
-
